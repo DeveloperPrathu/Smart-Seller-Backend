2b21f70529cacd273f46847e46de39ae3d4611b3
Prathu Gupta <developer.prathugupta@gmail.com>
2023-06-03 20:49:05 +0530

    Created create account api

--- core/backend/urls.py
+++ core/backend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from backend.views import request_otp
+from backend.views import request_otp, verify_otp, create_account
 
 urlpatterns = [
     path('request_otp/', request_otp),
+    path('verify_otp/', verify_otp),
+    path('create_account/', create_account),
 ]
--- core/backend/views.py
+++ core/backend/views.py
@@ -1,7 +1,11 @@
+import datetime
+
+from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
-from backend.models import User
+from backend.models import User, Otp
 from backend.utils import send_otp
 
 
@@ -18,3 +22,39 @@ def request_otp(request):
         return send_otp(phone)
     else:
         return Response('Data Missing', status=400)
+
+
+@api_view(['POST'])
+def verify_otp(request):
+    phone = request.data.get('phone')
+    otp = request.data.get('otp')
+
+    otp_obj = get_object_or_404(Otp, phone=phone, verified=False)
+
+    if otp_obj.validity.replace(tzinfo=None) > datetime.datetime.utcnow():
+        if otp_obj.otp == otp:
+            otp_obj.verified = True
+            otp_obj.save()
+            return Response('OTP_verified_successfully')
+        else:
+            return Response('Incorrect OTP', 404)
+    else:
+        return Response('OTP expired', 404)
+
+
+@api_view(['POST'])
+def create_account(request):
+    email = request.data.get('email')
+    phone = request.data.get('phone')
+    password = request.data.get('password')
+    fullname = request.data.get('fullname')
+
+    if email and phone and password and fullname:
+        otp_obj = get_object_or_404(Otp, phone=phone, verified=True)
+        otp_obj.delete()
+
+        User.objects.create(email=email, phone=phone, fullname=fullname, password=make_password(password))
+        return Response("account created_successfully!")
+
+    else:
+        return Response('data_missing', 400)
